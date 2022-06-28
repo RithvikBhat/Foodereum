f404335a765d2a53514f1f723f56b4a38f22cc91
Gautam Kumar <56199626+gautamK007@users.noreply.github.com>
2022-06-28 21:14:21 +0530

    Update FoodSupplyChain.sol

--- smartcontract/FoodSupplyChain.sol
+++ smartcontract/FoodSupplyChain.sol
@@ -21,15 +21,9 @@ contract FoodSupplyChain {
     mapping(uint => FoodProduct) allProducts;
     uint256 items=0;
     
-    function concat(string memory _a, string memory _b) public returns (string memory){
-        bytes memory bytes_a = bytes(_a);
-        bytes memory bytes_b = bytes(_b);
-        string memory length_ab = new string(bytes_a.length + bytes_b.length);
-        bytes memory bytes_c = bytes(length_ab);
-        uint k = 0;
-        for (uint i = 0; i < bytes_a.length; i++) bytes_c[k++] = bytes_a[i];
-        for (uint i = 0; i < bytes_b.length; i++) bytes_c[k++] = bytes_b[i];
-        return string(bytes_c);
+    function concat(string memory a,string memory b) public pure returns (string memory)
+    {
+        return string(abi.encodePacked(a,'',b));
     }
     
     function newItem(string memory _text, string memory _date) public returns (bool) {
