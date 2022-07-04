7a41730d8c2e4ef0b0bb763c2ff69ca848312373
Gautam Kumar <56199626+gautamK007@users.noreply.github.com>
2022-07-04 21:01:06 +0530

    Update and rename FoodSupplyChain.sol to Foodereum.sol

--- smartcontract/Foodereum.sol
+++ smartcontract/Foodereum.sol
@@ -1,15 +1,14 @@
 pragma solidity ^0.6.0;
 
-contract FoodSupplyChain {
-    
+contract Foodereum {
     event Added(uint256 index);
     
-    struct State{
+    struct State {
         string details;
         address user;
     }
     
-    struct FoodProduct{
+    struct FoodProduct {
         address creator;
         string productName;
         uint256 productId;
@@ -21,32 +20,27 @@ contract FoodSupplyChain {
     mapping(uint => FoodProduct) allProducts;
     uint256 items=0;
     
-    function concat(string memory a,string memory b) public pure returns (string memory)
-    {
+    function concat(string memory a,string memory b) public pure returns (string memory) {
         return string(abi.encodePacked(a,'',b));
     }
     
-    function newItem(string memory _text, string memory _date) public returns (bool) {
-        FoodProduct memory newItem = FoodProduct({creator: msg.sender, totalStates: 0,productName: _text, productId: items, date: _date});
-        allProducts[items]=newItem;
+    function sanction(string memory _text, string memory _date) public returns (bool) {
+        FoodProduct memory sanction = FoodProduct({creator: msg.sender, totalStates: 0,productName: _text, productId: items, date: _date});
+        allProducts[items]=sanction;
         items = items+1;
         emit Added(items-1);
         return true;
     }
     
-    function addState(uint _productId, string memory info) public returns (string memory) {
+    function update(uint _productId, string memory info) public returns (string memory) {
         require(_productId<=items);
-        
         State memory newState = State({user: msg.sender, details: info});
-        
         allProducts[_productId].positions[ allProducts[_productId].totalStates ]=newState;
-        
         allProducts[_productId].totalStates = allProducts[_productId].totalStates +1;
         return info;
     }
     
-    function searchProduct(uint _productId) public returns (string memory) {
-
+    function trackProduct(uint _productId) public returns (string memory) {
         require(_productId<=items);
         string memory output="<b>Product Name: </b>";
         output=concat(output, allProducts[_productId].productName);
@@ -56,9 +50,6 @@ contract FoodSupplyChain {
         for (uint256 j=0; j<allProducts[_productId].totalStates; j++){
             output=concat(output, allProducts[_productId].positions[j].details);
         }
-
         return output;
-        
     }
-    
 }
